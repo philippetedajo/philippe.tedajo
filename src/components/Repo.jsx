debccf943a0549116533396c4b556d9cbc0dde4f
phil <philmuse21@gmail.com>
2020-09-11 18:22:21 +0100

    update

--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import styled from "styled-components";
 import Sample from "../pictures/anothersample.jpg";
 import { FiGithub } from "react-icons/fi";
@@ -9,62 +9,60 @@ class Repo extends React.Component {
   render() {
     const { name, forks_count, stargazers_count } = this.props.repoData;
     return (
-      <CardRepo style={{ width: "15rem" }} className="bg-dark text-white">
-        <Card.Img src={Sample} alt="Card image" />
-        <CardOverlay>
-          <Info>
-            <Group>
-              <Card.Text className="mr-2">
-                <span className="mr-2">
-                  <StarIcon /> {stargazers_count}
-                </span>
-              </Card.Text>
-              <Card.Text className="mr-2">
-                <span className="mr-2">
-                  <RepoForkedIcon /> {forks_count}
-                </span>
-              </Card.Text>
-              <Card.Text>
-                <a
-                  href="https://github.com/philippetedajo"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  className="mr-2"
-                >
-                  <FiGithub />
-                </a>
-              </Card.Text>
-            </Group>
-            <RepoTitle className="mt-5">{name}</RepoTitle>
-          </Info>
-        </CardOverlay>
-      </CardRepo>
+      <Anchor
+        href="https://github.com/philippetedajo"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <Meta>
+          <span className="mr-2">
+            <StarIcon /> {stargazers_count}
+          </span>
+          <span className="mr-2">
+            <RepoForkedIcon /> {forks_count}
+          </span>
+          <span>
+            <FiGithub />
+          </span>
+        </Meta>
+        <div>{name}</div>
+      </Anchor>
     );
   }
 }
 
 export default Repo;
 
-const CardRepo = styled(Card)`
-  border: none;
-  border-radius: 15px !important;
-  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1);
+const Anchor = styled.a`
+  width: 15rem;
+  background: black;
+  height: 135px;
+  width: 240px;
   margin: 1rem;
-  & :hover {
-    box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
-  }
-`;
-const Info = styled.div`
+  text-decoration: none !important;
+  color: #fff;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1);
+  border-radius: 5px !important;
+  padding: 1rem;
+  &:hover {
+    box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+    color: #0994f0;
+  }
 `;
-const RepoTitle = styled(Card.Text)``;
-const CardOverlay = styled(Card.ImgOverlay)`
-  margin: 0;
-  padding: 8px;
-`;
-const Group = styled.div`
+
+const Meta = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
+
+/* 
+
+
+    -webkit-transition: color 1s cubic-bezier(.25,1,.25,1),background-color 1s cubic-bezier(.25,1,.25,1);
+    transition: color 1s cubic-bezier(.25,1,.25,1),background-color 1s cubic-bezier(.25,1,.25,1);
+
+
+*/
