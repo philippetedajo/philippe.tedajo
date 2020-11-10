705a9b558cad0579d22a6298a93d9581643f18b0
Philippe <Philimart21@gmail.com>
2020-11-10 17:39:30 +0100

    adding site structure and config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import React from "react"
-import "../styles/styles.scss"
-import "../static/fonts/fonts.scss"
-import { Home, About, Contact } from "../sections"
+import React from "react";
+import "../styles/styles.scss";
+import "../static/fonts/fonts.scss";
+import { Home, About, Contact } from "../sections";
 
-const Index = props => (
+const Index = () => (
   <>
     <Home />
     <About />
     <Contact />
   </>
-)
+);
 
-export default Index
+export default Index;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,35 +1,24 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import { MDXRenderer } from "gatsby-plugin-mdx"
-
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Bio from "../components/bio";
+import SEO from "../components/seo";
+import { rhythm, scale } from "../utils/typography";
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.mdx
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
+    const post = this.props.data.mdx;
+    const siteTitle = this.props.data.site.siteMetadata.title;
+    const { previous, next } = this.props.pageContext;
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <div className="container mt-5 pt-5" title={siteTitle}>
         <SEO
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
         <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
+        <p>{post.frontmatter.date}</p>
         <MDXRenderer>{post.body}</MDXRenderer>
         <hr
           style={{
@@ -62,12 +51,12 @@ class BlogPostTemplate extends React.Component {
             )}
           </li>
         </ul>
-      </Layout>
-    )
+      </div>
+    );
   }
 }
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -88,4 +77,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
