42486b13e6760d35fc2c1d3db1f562589499ef74
Philippe <Philimart21@gmail.com>
2020-12-01 12:11:26 +0100

    update

--- src/components/RepoSlider.js
+++ src/components/RepoSlider.js
@@ -3,9 +3,9 @@ import gsap, { Expo } from "gsap"
 
 const RepoSlider = data => {
   const slides = [
-    { number: "#00", title: "Lorem 0", imgSrc: "" },
-    { number: "#01", title: "Lorem 1", imgSrc: "" },
-    { number: "#02", title: "Lorem 2", imgSrc: "" },
+    { number: "#00", title: "Ideal", imgSrc: "" },
+    { number: "#01", title: "Nature ", imgSrc: "" },
+    { number: "#02", title: "Ability", imgSrc: "" },
   ]
 
   const [currentSilde, setCurrentSlide] = useState({
@@ -40,23 +40,39 @@ const RepoSlider = data => {
 
   //====================================================================================================
 
-  const hello = ["a", "b", "c"]
+  const titles = useRef(slides.map(() => createRef()))
+  const numbers = useRef(slides.map(() => createRef()))
 
-  const repoTitles = useRef(hello.map(() => createRef()))
+  const startAnimation = (a, b) => {
+    switch (animation) {
+      case "rigth":
+        gsap.from(a, {
+          duration: 1,
+          opacity: 0,
+          scale: 0.7,
+          x: 500,
+        })
+        gsap.from(b, {
+          duration: 1,
+          opacity: 0,
+          scale: 0.7,
+          x: 100,
+        })
 
-  const startAnimation = item => {
-    if (animation === "rigth") {
-      gsap.from(item, {
-        duration: 1,
-        scale: 0.7,
-        x: 500,
-      })
-    } else {
-      gsap.from(item, {
-        duration: 1,
-        scale: 0.7,
-        x: -500,
-      })
+        break
+      case "left":
+        gsap.from(a, {
+          duration: 1,
+          scale: 0.7,
+          x: -500,
+        })
+        gsap.from(b, {
+          duration: 1,
+          opacity: 0,
+          scale: 0.7,
+          x: -100,
+        })
+        break
     }
   }
 
@@ -64,18 +80,22 @@ const RepoSlider = data => {
 
   useEffect(() => {
     setCurrentSlide({ ...currentSilde, activeIndex })
-    const elements = repoTitles.current.map(ref => ref.current)
-    startAnimation(elements[activeIndex])
+    const title = titles.current.map(ref => ref.current)
+    const number = numbers.current.map(ref => ref.current)
+
+    startAnimation(title[activeIndex], number[activeIndex])
   }, [activeIndex])
 
   return (
     <div className="d-flex flex-column flex-md-row justify-content-end justify-content-md-between align-items-center h-100 repo-slider">
       {/* slider */}
       <div className={`box-slide`}>
-        <div className="repo-number"> {slides[activeIndex].number} </div>
+        <div className="repo-number" ref={numbers.current[activeIndex]}>
+          {slides[activeIndex].number}
+        </div>
         <div
-          className={`repo-title text-center ${activeIndex}`}
-          ref={repoTitles.current[activeIndex]}
+          className={`repo-title text-center`}
+          ref={titles.current[activeIndex]}
         >
           {slides[activeIndex].title}
         </div>
