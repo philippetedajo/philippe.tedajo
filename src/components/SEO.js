f2b228165db2542393e855d012b49403b45ccef5
Philippe <Philimart21@gmail.com>
2020-12-09 09:42:20 +0100

    Seo optimization

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,13 +3,69 @@ import { StaticQuery, graphql } from "gatsby"
 
 import React from "react"
 
-const SEO = ({ title, description, author, url, image, twitterUsername }) => {
+const SEO = ({
+  title,
+  description,
+  author,
+  url,
+  image,
+  twitterUsername,
+  keywords,
+}) => {
+  console.log(title)
   return (
     <StaticQuery
       query={detailsQuery}
       render={data => {
+        console.log(data)
+        const metaTitle = title || data.site.siteMetadata.title
         const metaDescription =
           description || data.site.siteMetadata.description
+        const metaAuthor = author || data.site.siteMetadata.author
+        const metaUrl = url || data.site.siteMetadata.url
+        const metaImage = image || data.site.siteMetadata.image
+        const metaTwitterUsername =
+          twitterUsername || data.site.siteMetadata.twitterUsername
+        const metaKeywords = keywords || ["Gatsby", "Portofolio"]
+
+        return (
+          <Helmet
+            title={metaTitle}
+            meta={[
+              {
+                name: `description`,
+                content: description,
+              },
+              {
+                name: `twitter:username`,
+                content: twitterUsername,
+              },
+              {
+                property: `og:title`,
+                content: metaTitle,
+              },
+              {
+                property: `og:title`,
+                content: metaTitle,
+              },
+              {
+                property: `og:description`,
+                content: metaDescription,
+              },
+              {
+                property: `og:url`,
+                content: metaUrl,
+              },
+            ].concat(
+              metaKeywords && metaKeywords > 0
+                ? {
+                    name: "keywords",
+                    content: metaKeywords.join(`, `),
+                  }
+                : []
+            )}
+          />
+        )
       }}
     />
   )
