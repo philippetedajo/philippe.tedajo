3307f00b1c82f1f48e0b802320fa99339fdb35ad
Philippe <Philimart21@gmail.com>
2021-01-07 20:06:26 +0100

    full update

--- src/sections/Project.js
+++ src/sections/Project.js
@@ -2,15 +2,19 @@ import React from "react"
 import "../styles/project.css"
 
 const Project = () => {
-  const Project = ["Lorem", "Lorem", "Lorem"]
+  const projects = [
+    { name: "Nextwind", link: "https://nextwind-snowy.vercel.app/" },
+    { name: "Reminder.", link: "https://reminderapp.netlify.app/" },
+    { name: "BookCart", link: "https://bookcart.netlify.app/" },
+  ]
 
   return (
     <div className="p-md-5">
       <h2 className="text-center text-lg-left mt-3 mt-lg-0">SOME PROJECTS</h2>
       <ul className="d-flex flex-column">
-        {Project.map(p => (
-          <a className="projet" href="">
-            Lorem ipsum
+        {projects.map(p => (
+          <a className="projet" href={p.link}>
+            {p.name}
           </a>
         ))}
       </ul>
