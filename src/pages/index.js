f1b714a56ca127131460b7995bb72288a7878f88
philippetedajo <philimart21@gmail.com>
2021-01-08 02:10:27 +0100

    adding noisy bg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React, { useRef, useEffect } from "react"
 import Splash from "../components/Splash"
+import Content from "../components/Reveal"
 import "../styles/styles.scss"
 import "../static/fonts/fonts.scss"
 import { Cursor, SEO } from "../components"
 import { Home, About, Project } from "../sections"
 
 const Index = () => {
+  const content = useRef(null)
   const wrapper = useRef(null)
   const loader = useRef(null)
 
   useEffect(() => {
     Splash(wrapper.current, loader.current)
+    Content(content.current)
   }, [])
 
   return (
@@ -22,7 +25,7 @@ const Index = () => {
           <span className="loader-inner" />
         </span>
       </div>
-      <div className="content fade-in">
+      <div ref={content}>
         <Home />
         <About />
         <Project />
