682d92b499a198087781b6f5ae561215a236411f
philippetedajo <philimart21@gmail.com>
2021-04-16 23:07:46 +0100

    update responsive

--- components/Sound.js
+++ components/Sound.js
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from "react";
 import ReactHowler from "react-howler";
+import axios from "axios";
 
 const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    setIsPlaying(true);
+    // setIsPlaying(true);
   }, []);
 
   const handleToggle = () => {
     setIsPlaying(!isPlaying);
   };
 
+  const getSong = async () => {
+    await axios
+      .get(
+        "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+      )
+      .then((res) => console.log(res));
+  };
+
   return (
     <div>
       <ReactHowler
-        src="https://opengameart.org/sites/default/files/level3.mp3"
-        // src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+        //src="https://opengameart.org/sites/default/files/level3.mp3"
+        src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
         playing={isPlaying}
         loop={true}
       />
