ff48967c0778ac55178cbebe6f796853f918744a
philippetedajo <philimart21@gmail.com>
2021-04-16 10:42:01 +0100

    update seo tags

--- pages/overview.js
+++ pages/overview.js
@@ -2,21 +2,29 @@ import Link from "next/link";
 import PageTemplate from "../templates/page.template";
 import { motion } from "framer-motion";
 import { container, text_reaveal, fade } from "../utils/Animation";
-import { Frame } from "../components"
+import { Frame } from "../components";
+import SeoTags from "../components/SeoTags";
 
 const Overview = () => {
-
   const overviews = [
- { content: {title:"Nextwind", link: "https://nextwind-snowy.vercel.app/"} },
- { content: {title:"Codetree", link: "https://codetree.vercel.app/"} },
- { content: {title:"Rt", link: "https://www.rosine.tedajo.com/"} },
+    {
+      content: {
+        title: "Nextwind",
+        link: "https://nextwind-snowy.vercel.app/",
+      },
+    },
+    { content: { title: "Codetree", link: "https://codetree.vercel.app/" } },
+    { content: { title: "Rt", link: "https://www.rosine.tedajo.com/" } },
   ];
 
   const overviewList = overviews.map((project, index) => (
     <div className="overflow-hidden mt-6" key={index}>
       <Link href={project.content.link}>
-        <a target="_blank" rel="noreferrer noopener" >
-          <motion.div className="flex transform duration-700 hover:scale-110 hover:text-third" variants={text_reaveal}>
+        <a target="_blank" rel="noreferrer noopener">
+          <motion.div
+            className="flex transform duration-700 hover:scale-110 hover:text-third"
+            variants={text_reaveal}
+          >
             <h1 className="mr-2">{project.content.title}</h1>
           </motion.div>
         </a>
@@ -25,19 +33,22 @@ const Overview = () => {
   ));
 
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      className="flex"
-    >
-      <motion.div variants={container} className="md:w-1/2 lg:pl-36 z-20">
-        {overviewList}
+    <>
+      <SeoTags />
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="flex"
+      >
+        <motion.div variants={container} className="md:w-1/2 lg:pl-36 z-20">
+          {overviewList}
+        </motion.div>
+        <div className=" opacity-20 md:opacity-100 md:static fixed md:block w-1/2">
+          <Frame radius="rounded-full" />
+        </div>
       </motion.div>
-      <div className=" opacity-20 md:opacity-100 md:static fixed md:block w-1/2">
-        <Frame radius="rounded-full"/>
-      </div>
-    </motion.div>
+    </>
   );
 };
 
