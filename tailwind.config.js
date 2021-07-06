e8da5064d33e1c292419c254422828b048644a74
Philippe <Philimart21@gmail.com>
2021-07-06 17:27:56 +0100

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: ["./pages/**/*.js", "./components/**/*.js", "./templates/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
