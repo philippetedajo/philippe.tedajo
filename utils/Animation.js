fb708792da4763bc54c739f272b70b52e576e814
Philippe <Philimart21@gmail.com>
2021-07-06 12:28:49 +0100

    up

--- utils/Animation.js
+++ utils/Animation.js
@@ -11,11 +11,10 @@ export const container = {
   },
 };
 
-
 export const text_reaveal = {
   initial: { y: 80 },
-  animate: { y: 0, opacity: 1, transition: { duration: 0.8 } },
-  exit: { y: 80,opacity: 0, transition: { duration: 0.8 } },
+  animate: { y: 0, opacity: 1, transition: { duration: 1.1 } },
+  exit: { y: 80, opacity: 0, transition: { duration: 1.1 } },
 };
 
 export const text_reaveal_fade = {
