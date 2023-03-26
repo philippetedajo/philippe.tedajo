370af50492175e7e191492719d1c1437957cea22
milliorn <scottmilliorn@gmail.com>
2023-03-26 16:57:43 -0700

    Links do not have a discernible name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,14 +11,14 @@ export const Footer = () => {
       <ul className="flex text-center ">
         <p className="mr-2.5 lg:mr-10 cursor-pointer">
           <Link href="https://github.com/philippetedajo">
-            <a target="_blank" rel="noreferrer noopener">
+            <a target="_blank" rel="noreferrer noopener" aria-label="github">
               <RiGithubFill size={20} className="hover:text-third" />
             </a>
           </Link>
         </p>
         <p className="mr-2.5 lg:mr-10 cursor-pointer">
           <Link href="https://www.linkedin.com/">
-            <a target="_blank" rel="noreferrer noopener">
+            <a target="_blank" rel="noreferrer noopener" aria-label="linkedin">
               <RiLinkedinFill size={20} className="hover:text-third" />
             </a>
           </Link>
